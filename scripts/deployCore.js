45b436c988f08484cf670eb553781f614372f83e
John <jooohn.eth@gmail.com>
2022-09-18 18:58:22 -0400

    Trying to deploy on Arbitrum

--- scripts/deployCore.js
+++ scripts/deployCore.js
@@ -2,15 +2,25 @@ const { ethers, network } = require("hardhat");
 
 const main = async () => {
   let baseAssetAddress;
+  let aggregatorAddress;
 
   if (network.name === "hardhat") {
     const MockERC20 = await ethers.getContractFactory("MockERC20");
     let baseAsset = await MockERC20.deploy("Mock Asset", "MOCK");
     baseAssetAddress = baseAsset.address;
+    aggregatorAddress = "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e";
     console.log(`Base asset mock address: ${baseAssetAddress}`);
+    console.log(`Chainlink price aggregator address: ${aggregatorAddress}`);
   } else if (network.name === "goerli") {
     baseAssetAddress = "0x11fE4B6AE13d2a6055C8D9cF65c55bac32B5d844";
+    aggregatorAddress = "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e";
     console.log(`Base asset address: ${baseAssetAddress}`);
+    console.log(`Chainlink price aggregator address: ${aggregatorAddress}`);
+  } else if (network.name === "arbitrum") {
+    baseAssetAddress = "";
+    aggregatorAddress = "0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612";
+    console.log(`Base asset address: ${baseAssetAddress}`);
+    console.log(`Chainlink price aggregator address: ${aggregatorAddress}`);
   }
 
   const FusionToken = await ethers.getContractFactory("FusionToken");
@@ -22,7 +32,8 @@ const main = async () => {
   const FusionCore = await ethers.getContractFactory("FusionCore");
   const fusionCore = await FusionCore.deploy(
     baseAssetAddress,
-    fusionToken.address
+    fusionToken.address,
+    aggregatorAddress
   );
   console.log(
     `Fusion Core address: ${fusionCore.address}, deployer: ${fusionCore.signer.address}`
